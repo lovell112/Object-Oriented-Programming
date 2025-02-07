6b5e9ca63c6ced672ccca6c3d148adb890744fd3
lovell112 <xuansontrannguyen06@gmail.com>
2025-02-08 00:58:45 +0700

    update v1.4.0 : add plane ticket

---
+++ planeTicket.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+using namespace std;
+
+struct dateTime{
+    int day;
+    int moonth;
+    int year;
+};
+
+class planeTicket {
+    private:
+        string name;
+        dateTime date;
+        int ticketPrice;
+    public:
+        planeTicket(){
+            name = "";
+            date.day = 0;
+            date.moonth = 0;
+            date.year = 0;
+            ticketPrice = 0;
+        }
+        planeTicket(string n, int dd, int mm, int yyyy, int tp){
+            name = n;
+            date.day = dd;
+            date.moonth = mm;
+            date.year = yyyy;
+            ticketPrice = tp;
+        }
+        void setName(string newName){
+            name = newName;
+        }
+        void setDate(int dd, int mm, int yyyy){
+            date.day = dd;
+            date.moonth = mm;
+            date.year = yyyy;
+        }
+        void setTicketPrice(int tp){
+            ticketPrice = tp;
+        }
+        const string& getName() const {
+            return name;
+        }
+        const dateTime& getDate() const {
+            return date;
+        }
+        const int& getTicketPrice() const {
+            return ticketPrice;
+        }
+};
+
+class person {
+    private:
+        string name;
+        int age;
+        string gender;
+    public:
+        person(){
+            name = "";
+            age = 0;
+            gender = "";
+        }
+        person(string n, int a, string g){
+            name = n;
+            age = a;
+            gender = g;
+        }
+        void setName(string newName){
+            name = newName;
+        }
+        void setAge(int newAge){
+            age = newAge;
+        }
+        void setGender(string newGender){
+            gender = newGender;
+        }
+        const string& getName() const {
+            return name;
+        }
+        const int& getAge() const {
+            return age;
+        }
+        const string& getGender() const {
+            return gender;
+        }
+};
+
+class passenger : public person {
+    private:
+        planeTicket ticket;
+        int numberTicket;
+    public:
+        passenger(string n, int a, string g, string nameTicket, int dd, int mm, int yyyy, int price, int number) : person(n, a, g){
+            ticket.setName(nameTicket);
+            ticket.setDate(dd, mm, yyyy);
+            ticket.setTicketPrice(price);
+            numberTicket = number;
+        }
+        passenger(){
+            ticket.setName("");
+            ticket.setDate(0, 0, 0);
+            ticket.setTicketPrice(0);
+            numberTicket = 0;
+        }
+        const int& getNumberTicket() const {
+            return numberTicket;
+        }
+        const planeTicket& getTicket() const {
+            return ticket;
+        }
+};
+
+int main(){
+    int numberPassenger; cin >> numberPassenger;
+    passenger* user = new passenger[numberPassenger];
+    for (int i = 0; i < numberPassenger; i++){
+        if(i == 0) cout << "Enter the " << i + 1 << "st : " << endl; 
+        else if(i == 1) cout << "Enter the " << i + 1 << "nd : " << endl; 
+        else if(i == 2) cout << "Enter the " << i + 1 << "rd : " << endl; 
+        else cout << "Enter the " << i + 1 << "th : " << endl;
+        cin.ignore();
+        string fullName; getline(cin, fullName);
+        int age; cin >> age;
+        cin.ignore();
+        string gender; getline(cin, gender);
+        string nameTicket; getline(cin, nameTicket);
+        int dd; cin >> dd;
+        int mm; cin >> mm;
+        int yyyy; cin >> yyyy;
+        int price; cin >> price;
+        int number; cin >> number;
+        passenger* T = new passenger(fullName, age, gender, nameTicket, dd, mm, yyyy, price, number);
+        user[i] = *T;
+        delete T;
+    }
+    for(int i = 0; i < numberPassenger; i++){
+        if(i == 0) cout << "The " << i + 1 << "st : "; 
+        else if(i == 1) cout << "The " << i + 1 << "nd : "; 
+        else if(i == 2) cout << "The " << i + 1 << "rd : "; 
+        else cout << "The " << i + 1 << "th : ";
+        cout << user[i].getName() << " needs to pay : " << user[i].getTicket().getTicketPrice() * user[i].getNumberTicket() << "$" << endl;
+    }
+    delete[] user;
+    return 0;
+}
